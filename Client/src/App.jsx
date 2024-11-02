c02fdffe72a59635b5635b7b8d5827d8dd7431bf
duy49725 <duy49725@gmail.com>
2024-11-02 09:15:09 +0700

    createheader[ahge

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,10 +6,12 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import Project from './pages/Project'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/about' element={<About />} />
